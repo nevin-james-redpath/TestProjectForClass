a57a11537a2d0fac523d125f307372d649fca931
nevin-james-redpath <nevin.james@redpathmining.com>
2025-09-24 17:03:36 -0400

    Commit 2nd

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         //First commit test
         System.out.println("Hello world!");
-
+        System.out.println("Hello World 2nd");
     }
 }
