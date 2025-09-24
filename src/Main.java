626ce5797c30121a7aea56365450d6c7204346ec
nevin-james-redpath <nevin.james@redpathmining.com>
2025-09-24 17:02:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Type
+ * Docs
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        //First commit test
+        System.out.println("Hello world!");
+
+    }
+}
